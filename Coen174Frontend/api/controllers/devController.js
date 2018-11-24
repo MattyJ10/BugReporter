e09caeb81c905a9744464627e8ac7b35419196b2
MattyJ10 <jasaitismatt@yahoo.com>
2018-11-24 13:51:53 -0800

    t

--- Coen174Frontend/api/controllers/devController.js
+++ Coen174Frontend/api/controllers/devController.js
@@ -2,13 +2,14 @@ const mongoose = require('mongoose');
 const user = require('../models/bug.js'); 
 
 module.exports.getAllDevs = function(req, res) {
-	user.find.or([{position: "developer"}, {position: "tester"}]).then((err, data) => {
+	user.find({ $or: [{position: "developer"}, {position: "tester"}]}).exec((err, data) => {
 		if (err) {
 			res.status(400).send({
 				error: true,
 				msg: "Could not get devs"
 			})
 		} else {
+			console.log(data);
 			res.status(200).send({
 				data: data
 			})
